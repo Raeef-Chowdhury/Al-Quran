a63d018079a1e434cf1b450151c8fcfdc8150633
Raeef Chowdhury <raeef.chowdhury1@education.nsw.gov.au>
2025-11-04 22:16:39 +1100

    complete DUA Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import SurahSection from "./Components/SurahSection";
 import DuaSection from "./Components/DuaSection";
 import { useState, useEffect } from "react";
 
-//TODO: DAILY DUA
-//TODO:SECTION OF DUAS https://github.com/fitrahive/dua-dhikr
-//TODO:Recitation Page
-//TODO: REFACTOR HERO/HEader
+//TODO:Recitation Page=
 //TODO:Sticky Nav
 //TODO: CTA HERo
-//TODO: Bookmark surah ayah
+//TODO: Bookmark surah & dua
+//TODO: Recitation Dua & Surah
 //TODO: Add Widgets Namaz Info
 
 function App() {
--- src/Components/DuaPage.jsx
+++ src/Components/DuaPage.jsx
@@ -1,22 +1,64 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */ const excludeArr = [
+  "Bathroom",
+  "Mosque",
+  "Eating",
+  "Sleep",
+  "General",
+  "Protection",
+  "Home",
+  "Travel",
+  "Weather",
+];
 import Header from "./Header";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import duas from "../data/daily-duas.json";
 function DuaPage() {
   const [dua, setDua] = useState([]);
+  const [allDuas, setAllDuas] = useState([]);
+  const [bg, setBg] = useState("all");
   useEffect(() => {
     const dua = duas;
 
     setDua(dua);
+    setAllDuas(dua);
   }, []);
   const [duaSearch, setDuaSearch] = useState("");
+  const filterSearch = (search) => {
+    const filtered = duas.filter(
+      (d) =>
+        d.title.toLowerCase().includes(search.toLowerCase()) ||
+        d.translation.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setDua(filtered);
+  };
+  useEffect(() => {
+    filterSearch(duaSearch);
+  }, [duaSearch]);
+  function setCategory(category) {
+    const filter = allDuas.filter((d) => d.category === category);
+    setBg(category);
+    setDua(filter);
+  }
+  function setOther() {
+    const filter = allDuas.filter((d) => !excludeArr.includes(d.category));
+    console.log(filter);
+    setBg("Other");
+    setDua(filter);
+  }
+  function setAll() {
+    const filtered = allDuas.filter((s) => s.number > 0);
+    setBg("All");
+    setDua(filtered);
+  }
   return (
     <>
       <Header />
 
       <main className="max-w-[1720px] flex flex-col items-center mx-auto surah__reading  w-[fit-content] h-[fit-content]">
-        <div className="flex flex-col items-center gap-[2.4rem]">
+        <div className="flex flex-col  items-center gap-[2.4rem]">
           <h1 className="text-shade text-[6.4rem] border-b-amber-500 border-b-8 mt-[4.8rem] ">
             All Duas
           </h1>
@@ -24,40 +66,45 @@ function DuaPage() {
             Reflect on the most important duas of the Quran
           </p>
           <input
-            className="text-slate-300  text-[2rem] py-[0.8rem] mt-[3.6rem] w-full outline-none min-w-[80rem] w-full mx-auto   flex-1  px-[2rem] bg-slate-700 border-3  border-shade  rounded-xl "
-            id="searchbar"
-            type="text"
-            placeholder="Search by name,number or translation here..."
+            className="text-slate-300  text-[2rem] py-[0.8rem] mt-[3.6rem]  outline-none w-[80rem]  mx-auto     px-[2rem] bg-slate-700 border-3  border-shade  rounded-xl "
+            id="text"
+            placeholder="Search by name or translation here..."
             value={duaSearch}
             onChange={(e) => setDuaSearch(e.target.value)}
           />
-          <div className="flex items-center justify-between w-[80%] bg-text border-shade border-4 p-0 rounded-full overflow-hidden">
+          <div className="flex mt-[3.6rem] mb-[7.2rem] items-center justify-center items-center flex-wrap gap-[1.8rem] w-[120rem] ">
             <button
-              className={`flex-1 text-[2.4rem]  text-primary py-[0.8rem] hover:bg-background transition-all duration-300 hover:cursor-pointer`}
+              onClick={() => setAll()}
+              className={` text-[2rem] ${
+                bg === "All" ? "bg-gradient-to-br  from-shade  to-primary" : ""
+              }  text-text py-[0.8rem] hover:bg-gradient-to-br  from-shade  to-primary hover:bg-primary text   min-w-[18rem]  rounded-full overflow-hidden bg-shade/10 transition-all duration-300 hover:cursor-pointer`}
             >
               All
             </button>
+            {excludeArr.map((category) => (
+              <CategoryButton
+                key={category}
+                bg={bg}
+                setCategory={setCategory}
+                category={category}
+              />
+            ))}
             <button
-              className={`flex-1 text-[2.4rem] text-primary py-[0.8rem] hover:bg-background transition-all duration-300 hover:cursor-pointer`}
+              onClick={() => setOther()}
+              className={` text-[2rem] ${
+                bg === "Other"
+                  ? "bg-gradient-to-br  from-shade  to-primary"
+                  : ""
+              } text-text py-[0.8rem] hover:bg-gradient-to-br  from-shade  to-primary hover:bg-primary text   min-w-[18rem]  rounded-full overflow-hidden bg-shade/10 transition-all duration-300 hover:cursor-pointer`}
             >
-              Short
-            </button>
-            <button
-              className={`flex-1 text-[2.4rem]  text-primary py-[0.8rem] hover:bg-background transition-all duration-300 hover:cursor-pointer`}
-            >
-              Medium
-            </button>
-            <button
-              className={`flex-1 text-[2.4rem] text-primary py-[0.8rem] hover:bg-background transition-all duration-300 hover:cursor-pointer`}
-            >
-              Long
+              Other
             </button>{" "}
           </div>
           <p className=" text-[2rem] text-text">
-            Showing (dualegth) of 34 Duas
+            Showing {dua.length} of 68 Duas
           </p>
         </div>
-        <ul className="mt-[7.2rem] grid grid-cols-2 gap-[6rem]  max-w-[1600px] mx-auto w-full">
+        <ul className="mt-[7.2rem] grid grid-cols-4 gap-[6rem]  max-w-[1600px] mx-auto w-full">
           {dua.map((dua) => (
             <DuaCard key={dua.number} dua={dua} />
           ))}
@@ -93,12 +140,55 @@ function DuaCard({ dua }) {
   return (
     <>
       <Link to={`/duas/${dua.number}`}>
-        <div className="max-w-[480px] min-h-[240px] mx-auto group relative shadow-2xl p-[1rem] rounded-2xl bg-primary/10 border border-primary/30 hover:bg-primary/20 hover:border-primary/60 transition-all duration-300">
-          <div className="flex flex-col gap-[0.8rem] justify-center items-center ">
-            <div className="w-20 h-20  rounded-2xl bg-gradient-to-br mt-[2.4rem] from-teal-500 to-teal-600  flex items-center justify-center shadow-lg transition-all duration-300 transform ">
-              <span className="text-white text-[3.2rem] font-bold  ">
-                {dua.number}
+        <div className="max-w-[480px] h-[240px] mx-auto group relative shadow-2xl p-[1rem] rounded-2xl bg-primary/10 border border-primary/30 hover:bg-primary/20 hover:border-primary/60 transition-all duration-300">
+          <div className="flex flex-col gap-[1.6rem] justify-center items-center ">
+            <div className="mt-[1.6rem]  gap-[0.8rem] bg-primary text-shade font-bold rounded-full px-[2.4rem] py-[0.6rem] flex items-center justify-center text-[1.8rem] shadow-md uppercase tracking-wide">
+              <span>
+                {dua.category === "Home"
+                  ? "🏠"
+                  : dua.category === "Sleep"
+                  ? "😴"
+                  : dua.category === "Morning"
+                  ? "🌅"
+                  : dua.category === "Bathroom"
+                  ? "🚿"
+                  : dua.category === "Eating"
+                  ? "🍽️"
+                  : dua.category === "Mosque"
+                  ? "🕌"
+                  : dua.category === "Ablution"
+                  ? "💧"
+                  : dua.category === "Fasting"
+                  ? "🌙"
+                  : dua.category === "Travel"
+                  ? "✈️"
+                  : dua.category === "Clothing"
+                  ? "👕"
+                  : dua.category === "Weather"
+                  ? "🌦️"
+                  : dua.category === "General"
+                  ? "📿"
+                  : dua.category === "Difficulty"
+                  ? "🤲"
+                  : dua.category === "Financial"
+                  ? "💰"
+                  : dua.category === "Social Etiquette"
+                  ? "🤝"
+                  : dua.category === "Protection"
+                  ? "🛡️"
+                  : dua.category === "Prayer"
+                  ? "🕌"
+                  : dua.category === "Health"
+                  ? "🏥"
+                  : dua.category === "Knowledge"
+                  ? "📚"
+                  : dua.category === "Family"
+                  ? "👨‍👩‍👧‍👦"
+                  : dua.category === "Death"
+                  ? "🕊️"
+                  : "📖"}
               </span>
+              {dua.category}
             </div>
             <h2 className="text-[1.8rem] font-bold text-shade mt-[2rem] text-center ">
               {dua.title}
@@ -108,7 +198,7 @@ function DuaCard({ dua }) {
           <p className="text-[1.6rem]  leading-[1.8] text-shade font-arabic mb-[3rem] text-center">
             {dua.arabic.length > 20 ? (
               <>
-                {dua.arabic.slice(0, 100)}
+                {dua.arabic.slice(0, 50)}
                 <span className="text-[3.2rem] text-center ml-[0.4rem]">
                   .....
                 </span>
@@ -121,6 +211,20 @@ function DuaCard({ dua }) {
           <span className="absolute bottom-0 left-0 w-full h-[1px] bg-primary/10 group-hover:bg-primary/40 transition-colors"></span>
         </div>
       </Link>
+    </>
+  );
+}
+function CategoryButton({ bg, setCategory, category }) {
+  return (
+    <>
+      <button
+        onClick={() => setCategory(category)}
+        className={` text-[2rem] ${
+          bg === category ? "bg-gradient-to-br  from-shade  to-primary" : ""
+        } text-text py-[0.8rem] hover:bg-gradient-to-br  from-shade  to-primary hover:bg-primary text   min-w-[18rem]  rounded-full overflow-hidden bg-shade/10 transition-all duration-300 hover:cursor-pointer`}
+      >
+        {category}
+      </button>
     </>
   );
 }
